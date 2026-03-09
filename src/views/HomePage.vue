ffba3c4eaf9f3ab9f1b34eb5c25461c6dc75b955
Britney Balderas <camobrit@gmail.com>
2026-03-09 17:38:16 -0500

    .

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,5 +1,7 @@
 <script setup></script>
 
 <template>
-  <main class="flex flex-1 items-center justify-center">Hello There</main>
+  <main
+    class="min-h-screen bg-[radial-gradient(circle_at_15%_20%,#334155,transparent_40%),radial-gradient(circle_at_85%_0%,#0f766e,transparent_28%),linear-gradient(160deg,#020617,#0f17sa,#111827)] px-4 py-8 text-slate-100 sm:px-6 lg:px-8"
+  ></main>
 </template>
